09370ccdd36cfedfaddd23bd0a69f725471d1eac
Siva Nalluri <glpat-wMt3yxgS2YUhS7cNuTfF>
2024-10-30 10:01:39 -0400

    Add product list component

--- Assignmnets/Project1/src/app/app.component.ts
+++ Assignmnets/Project1/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserListComponent } from './components/user-list/user-list.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    UserListComponent
+    UserListComponent,
+    ProductListComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
